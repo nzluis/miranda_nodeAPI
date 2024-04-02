b2f29f32579fd90ac4a246d72aca24cddef55884
luis <luisnavarroFS@gmail.com>
2024-04-02 21:26:00 +0200

    feat: persistent user changes

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
-import usersJson from '../data/users.json'
 import { UserData } from '../interfaces/User'
 import { delay } from '../utils/delay'
+import { addNew, deleteOne, fetchAll, fetchOne, updateOne } from '../services/dataServices'
 
 type UserResponse = {
     data: UserData | UserData[] | undefined
@@ -10,7 +10,7 @@ type UserResponse = {
 
 export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const response: UserResponse = await delay({ data: usersJson, ok: true })
+        const response: UserResponse = await delay({ data: fetchAll('users'), ok: true })
         if (!response.ok) res.status(502).send('Data not found')
         res.json(response.data)
     } catch (error) {
@@ -20,48 +20,57 @@ export const getUsers = async (req: Request, res: Response, next: NextFunction) 
 export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const id = req.params.id
-        const user: UserData | undefined = usersJson.find(user => user.id === id)
-        const response = await delay({ data: user as UserData, ok: true })
+        const response = await delay({ data: fetchOne(id, 'users'), ok: true })
         if (!response.ok) res.status(500).send('Not possible to access DB')
-        if (!response.data) res.status(404).send('User ID not found')
-        res.json(response.data)
+        else if (!response.data) res.status(404).send('User ID not found')
+        else res.json(response.data)
     } catch (error) {
         next(error)
     }
 }
-export const createUser = async (req: Request, res: Response, next: NextFunction) => {
+export const createUser = (req: Request, res: Response, next: NextFunction) => {
     try {
-        // const userAfterCreate = usersJson.concat(req.body)
-        const response: UserResponse = await delay({ data: req.body, ok: true })
-        if (!response.ok) res.status(500).send('Not possible to access DB')
-        console.log('Successfully created')
-        res.send(response.data)
+        const response: UserResponse = { data: req.body, ok: true }
+        if (!addNew(req.body, 'users')) res.send('ID already exists')
+        else if (!response.ok) {
+            res.status(500).send('Not possible to access DB')
+        }
+        else {
+            console.log('Successfully created')
+            res.send(response.data)
+        }
     } catch (error) {
         next(error)
     }
 }
-export const editUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const id = req.params.id
-        if (!usersJson.some(user => user.id === id)) res.send('Unknown ID')
-        // const usersAfterEdit = usersJson.map(user => user.id === id ? req.body : user)
-        const response: UserResponse = await delay({ data: req.body, ok: true })
-        if (!response.ok) res.status(500).send('Not possible to access DB')
-        console.log('Successfully edited')
-        res.json(response.data)
-    } catch (error) {
-        next(error)
-    }
-}
-export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+export const editUser = (req: Request, res: Response, next: NextFunction) => {
     try {
         const id = req.params.id
-        if (!usersJson.some(user => user.id === id)) res.send('Unknown ID')
-        // const usersAfterDelete = usersJson.filter(user => user.id !== id)
-        const response: UserResponse = await delay({ data: id, ok: true })
-        if (!response.ok) res.status(500).send('Not possible to access DB')
-        console.log('Successfully deleted')
-        res.json(response.data)
+        const response: UserResponse = { data: req.body, ok: true }
+        if (!updateOne(id, 'users', req.body)) res.send('Unknown ID')
+        else if (!response.ok) {
+            res.status(500).send('Not possible to access DB')
+        }
+        else {
+            console.log('Successfully edited')
+            res.json(response.data)
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+export const deleteUser = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = req.params.id
+        const response: { data: UserData, ok: boolean } = { data: req.body, ok: true }
+        if (!deleteOne(id, 'users')) res.send('Unknown ID')
+        else if (!response.ok) {
+            res.status(500).send('Not possible to access DB')
+        }
+        else {
+            console.log('Successfully deleted')
+            res.json(response.data!.id)
+        }
     } catch (error) {
         next(error)
     }

38333b05f8ee5e1520da806fa50f42dbecf0272b
luis <luisnavarroFS@gmail.com>
2024-04-02 21:25:46 +0200

    feat: persistent room changes

--- src/controllers/room.ts
+++ src/controllers/room.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
-import roomsJson from '../data/rooms.json'
 import { RoomData } from '../interfaces/Room'
 import { delay } from '../utils/delay'
+import { addNew, deleteOne, fetchAll, fetchOne, updateOne } from '../services/dataServices'
 
 type RoomResponse = {
     data: RoomData | RoomData[] | undefined
@@ -10,7 +10,7 @@ type RoomResponse = {
 
 export const getRooms = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const response: RoomResponse = await delay({ data: roomsJson, ok: true })
+        const response: RoomResponse = await delay({ data: fetchAll('rooms'), ok: true })
         if (!response.ok) res.status(502).send('Data not found')
         res.json(response.data)
     } catch (error) {
@@ -20,48 +20,57 @@ export const getRooms = async (req: Request, res: Response, next: NextFunction) 
 export const getRoomById = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const id = req.params.id
-        const room: RoomData | undefined = roomsJson.find(room => room.id === id)
-        const response = await delay({ data: room as RoomData, ok: true })
+        const response = await delay({ data: fetchOne(id, 'rooms'), ok: true })
         if (!response.ok) res.status(500).send('Not possible to access DB')
-        if (!response.data) res.status(404).send('Room ID not found')
-        res.json(response.data)
+        else if (!response.data) res.status(404).send('Room ID not found')
+        else res.json(response.data)
     } catch (error) {
         next(error)
     }
 }
-export const createRoom = async (req: Request, res: Response, next: NextFunction) => {
+export const createRoom = (req: Request, res: Response, next: NextFunction) => {
     try {
-        // const roomAfterCreate = roomsJson.concat(req.body)
-        const response: RoomResponse = await delay({ data: req.body, ok: true })
-        if (!response.ok) res.status(500).send('Not possible to access DB')
-        console.log('Successfully created')
-        res.send(response.data)
+        const response: RoomResponse = { data: req.body, ok: true }
+        if (!addNew(req.body, 'rooms')) res.send('ID already exists')
+        else if (!response.ok) {
+            res.status(500).send('Not possible to access DB')
+        }
+        else {
+            console.log('Successfully created')
+            res.send(response.data)
+        }
     } catch (error) {
         next(error)
     }
 }
-export const editRoom = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const id = req.params.id
-        if (!roomsJson.some(room => room.id === id)) res.send('Unknown ID')
-        // const roomsAfterEdit = roomsJson.map(room => room.id === id ? req.body : room)
-        const response: RoomResponse = await delay({ data: req.body, ok: true })
-        if (!response.ok) res.status(500).send('Not possible to access DB')
-        console.log('Successfully edited')
-        res.json(response.data)
-    } catch (error) {
-        next(error)
-    }
-}
-export const deleteRoom = async (req: Request, res: Response, next: NextFunction) => {
+export const editRoom = (req: Request, res: Response, next: NextFunction) => {
     try {
         const id = req.params.id
-        if (!roomsJson.some(room => room.id === id)) res.send('Unknown ID')
-        // const roomsAfterDelete = roomsJson.filter(room => room.id !== id)
-        const response: RoomResponse = await delay({ data: id, ok: true })
-        if (!response.ok) res.status(500).send('Not possible to access DB')
-        console.log('Successfully deleted')
-        res.json(response.data)
+        const response: RoomResponse = { data: req.body, ok: true }
+        if (!updateOne(id, 'rooms', req.body)) res.send('Unknown ID')
+        else if (!response.ok) {
+            res.status(500).send('Not possible to access DB')
+        }
+        else {
+            console.log('Successfully edited')
+            res.json(response.data)
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+export const deleteRoom = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = req.params.id
+        const response: { data: RoomData, ok: boolean } = { data: req.body, ok: true }
+        if (!deleteOne(id, 'rooms')) res.send('Unknown ID')
+        else if (!response.ok) {
+            res.status(500).send('Not possible to access DB')
+        }
+        else {
+            console.log('Successfully deleted')
+            res.json(response.data!.id)
+        }
     } catch (error) {
         next(error)
     }

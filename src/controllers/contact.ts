81d73172279a1ae72211de4b907301846c441987
luis <luisnavarroFS@gmail.com>
2024-04-04 19:00:02 +0200

    refactor: controller and service

--- src/controllers/contact.ts
+++ src/controllers/contact.ts
@@ -4,14 +4,14 @@ import { delay } from '../utils/delay'
 import { addNew, deleteOne, fetchAll, fetchOne, updateOne } from '../services/dataServices'
 
 type ContactResponse = {
-    data: ContactData | ContactData[] | undefined
+    data: ContactData | ContactData[] | null
     ok: boolean
 }
 
 export const getContacts = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const response: ContactResponse = await delay({ data: fetchAll('contacts'), ok: true })
-        if (!response.ok) res.status(502).send('Data not found')
+        if (!response.ok) res.status(404).json({ error: true, message: 'Data not found' })
         res.json(response.data)
     } catch (error) {
         next(error)
@@ -20,36 +20,29 @@ export const getContacts = async (req: Request, res: Response, next: NextFunctio
 export const getContactById = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const id = req.params.id
-        const response = await delay({ data: fetchOne(Number(id), 'contacts'), ok: true })
-        if (!response.ok) res.status(500).send('Not possible to access DB')
-        else if (!response.data) res.status(404).send('Contact ID not found')
+        const response: ContactResponse = await delay(fetchOne(id, 'contacts'))
+        if (!response.ok) res.status(404).json({ error: true, message: 'Contact ID not found' })
         else res.json(response.data)
     } catch (error) {
         next(error)
     }
 }
-export const createContact = (req: Request, res: Response, next: NextFunction) => {
+export const createContact = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const response: ContactResponse = { data: req.body, ok: true }
-        if (!addNew(req.body, 'contacts')) res.send('ID already exists')
-        else if (!response.ok) {
-            res.status(500).send('Not possible to access DB')
-        }
-        else {
+        const response: ContactResponse = await delay(addNew(req.body, 'contacts'))
+        if (!response.ok) {
+            res.status(409).send('ID already exists')
             console.log('Successfully created')
-            res.send(response.data)
         }
+        else res.json(response.data)
     } catch (error) {
         next(error)
     }
 }
-export const editContact = (req: Request, res: Response, next: NextFunction) => {
+export const editContact = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const response: ContactResponse = { data: req.body, ok: true }
-        if (!updateOne(req.body, 'contacts')) res.send('Unknown ID')
-        else if (!response.ok) {
-            res.status(500).send('Not possible to access DB')
-        }
+        const response: ContactResponse = await delay(updateOne(req.body, 'contacts'))
+        if (!response.ok) res.status(404).json({ error: true, message: 'Contact ID not found' })
         else {
             console.log('Successfully edited')
             res.json(response.data)
@@ -58,17 +51,14 @@ export const editContact = (req: Request, res: Response, next: NextFunction) => 
         next(error)
     }
 }
-export const deleteContact = (req: Request, res: Response, next: NextFunction) => {
+export const deleteContact = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const id = req.params.id
-        const response: { data: ContactData, ok: boolean } = { data: req.body, ok: true }
-        if (!deleteOne(Number(id), 'contacts')) res.send('Unknown ID')
-        else if (!response.ok) {
-            res.status(500).send('Not possible to access DB')
-        }
+        const response: ContactResponse = await delay(deleteOne(id, 'contacts'))
+        if (!response.ok) res.status(404).json({ error: true, message: 'Contact ID not found' })
         else {
             console.log('Successfully deleted')
-            res.json(response.data!.id)
+            res.json(response.data)
         }
     } catch (error) {
         next(error)

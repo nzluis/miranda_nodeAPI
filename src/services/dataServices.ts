06f50b9ebb51c657662b3e0043da5b6a278f0f91
luis <luisnavarroFS@gmail.com>
2024-04-05 04:48:49 +0200

    feat: add /tmp

--- src/services/dataServices.ts
+++ src/services/dataServices.ts
@@ -12,40 +12,40 @@ type DataResponse = {
 }
 
 export const fetchAll = (filename: string): { data: ArrayOfData, ok: boolean } => {
-    const data: ArrayOfData = JSON.parse(readFileSync(`./data/${filename}.json`, 'utf-8'))
+    const data: ArrayOfData = JSON.parse(readFileSync(`./tmp/${filename}.json`, 'utf-8'))
     if (!data || data.length === 0) return { data: [], ok: false }
     return { data, ok: true }
 }
 
 export const fetchOne = (id: string | number, filename: string): DataResponse => {
-    const data = JSON.parse(readFileSync(`./data/${filename}.json`, 'utf-8'))
+    const data = JSON.parse(readFileSync(`./tmp/${filename}.json`, 'utf-8'))
         .find((element: Data) => element.id.toString() === id)
     if (!data) return { data: null, ok: false }
     return { data, ok: true }
 }
 
 export const addNew = (newAdded: Data, filename: string): DataResponse => {
-    const data = JSON.parse(readFileSync(`./data/${filename}.json`, 'utf-8'))
+    const data = JSON.parse(readFileSync(`./tmp/${filename}.json`, 'utf-8'))
     if (data.some((element: Data) => element.id === newAdded.id)) return { data: null, ok: false }
     const dataAfterAddNew = data.concat(newAdded)
-    writeFileSync(`./data/${filename}.json`, JSON.stringify(dataAfterAddNew))
+    writeFileSync(`./tmp/${filename}.json`, JSON.stringify(dataAfterAddNew))
     return { data: newAdded, ok: true }
 }
 
 export const updateOne = (updatedData: Data, filename: string): DataResponse => {
-    const data: ArrayOfData = JSON.parse(readFileSync(`./data/${filename}.json`, 'utf-8'))
+    const data: ArrayOfData = JSON.parse(readFileSync(`./tmp/${filename}.json`, 'utf-8'))
     if (!data.some((element: Data) => element.id === updatedData.id)) return { data: null, ok: false }
     const dataAfterEdit = data.map((element: Data) => element.id === updatedData.id ? updatedData : element)
-    writeFileSync(`./data/${filename}.json`, JSON.stringify(dataAfterEdit))
+    writeFileSync(`./tmp/${filename}.json`, JSON.stringify(dataAfterEdit))
     return { data: updatedData, ok: true }
 }
 
 export const deleteOne = (id: string | number, filename: string): DataResponse => {
-    const data: ArrayOfData = JSON.parse(readFileSync(`./data/${filename}.json`, 'utf-8'))
-    const deletedData: Data = JSON.parse(readFileSync(`./data/${filename}.json`, 'utf-8'))
+    const data: ArrayOfData = JSON.parse(readFileSync(`./tmp/${filename}.json`, 'utf-8'))
+    const deletedData: Data = JSON.parse(readFileSync(`./tmp/${filename}.json`, 'utf-8'))
         .find((element: Data) => element.id.toString() === id)
     if (!deletedData) return { data: null, ok: false }
     const dataAfterDelete = data.filter((element: Data) => element.id.toString() !== id)
-    writeFileSync(`./data/${filename}.json`, JSON.stringify(dataAfterDelete))
+    writeFileSync(`./tmp/${filename}.json`, JSON.stringify(dataAfterDelete))
     return { data: deletedData, ok: true }
 }

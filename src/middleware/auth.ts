d77b29efa4b13fe4caaede4f75be003d6f353224
luis <luisnavarroFS@gmail.com>
2024-04-05 13:10:00 +0200

    fix: throw error & try-catch

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,13 +2,17 @@ import { NextFunction, Request, Response } from 'express'
 import jwt = require('jsonwebtoken')
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const secretKey: string = process.env.SECRET_KEY || ''
+    const secretKey = process.env.SECRET_KEY
+    if (!secretKey) throw new Error('Secret Key is undefined')
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) return res.sendStatus(401)
-
-    jwt.verify(token, secretKey, (err: any) => {
-        if (err) return res.sendStatus(403)
-        next()
-    })
+    try {
+        jwt.verify(token, secretKey, (err: any) => {
+            if (err) return res.sendStatus(403)
+            next()
+        })
+    } catch (error) {
+        next(error)
+    }
 }

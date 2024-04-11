1b27c6b6e1df0a49b1535d45d0f589976bc44bbd
luis <luisnavarroFS@gmail.com>
2024-04-11 15:07:56 +0200

    fix: hashpass if compare true

--- src/utils/checkPassword.ts
+++ src/utils/checkPassword.ts
@@ -3,8 +3,12 @@ import { UserData } from "../interfaces/User";
 
 export const checkPassword = (dataToUpdate: UserData, storedData: UserData) => {
     if (bcrypt.compareSync(dataToUpdate.password, storedData.password)) {
-        return dataToUpdate
+        return {
+            ...dataToUpdate,
+            password: storedData.password
+        }
     } else {
+        console.log('New Password Created')
         return {
             ...dataToUpdate,
             password: bcrypt.hashSync(dataToUpdate.password, bcrypt.genSaltSync(10))

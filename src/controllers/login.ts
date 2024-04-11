add1c1b1e4235efa6b48fc14aa51638618a76f82
luis <luisnavarroFS@gmail.com>
2024-04-11 13:50:32 +0200

    refactor: update login ApiError

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,22 +1,14 @@
 import { NextFunction, Request, Response } from 'express'
-import jwt = require('jsonwebtoken')
 import express from 'express'
-import bcrypt from "bcryptjs";
 import { getUser } from '../services/login';
-import { UserData } from '../interfaces/User';
 
 export const loginRouter = express.Router()
 
 loginRouter.post('/', async (req: Request, res: Response, next: NextFunction) => {
     const { email, password }: { email: string, password: string } = req.body
     try {
-        const user: UserData | null = await getUser(email)
-        if (!user) res.status(404).json({ error: true, message: 'User not found' })
-        else if (bcrypt.compareSync(password, user.password)) {
-            const token = jwt.sign({ email: user.email, password: user.password }, process.env.SECRET_KEY as string)
-            res.json({ AUTH_OK: 'Login successfully', token: token })
-        }
-        else res.status(403).json({ error: true, message: 'Incorrect Authentication' })
+        const authOk = await getUser(email, password)
+        res.status(200).json({ authOk })
     } catch (error) {
         next(error)
     }
--- src/services/login.ts
+++ src/services/login.ts
@@ -1,5 +1,14 @@
 import { User } from "../models/User"
+import { ApiError } from "../utils/handleErrors"
+import jwt = require('jsonwebtoken')
+import bcrypt from "bcryptjs";
 
-export const getUser = (email: string) => {
-    return User.findOne({ email })
+export const getUser = async (email: string, password: string) => {
+    const user = await User.findOne({ email })
+    if (!user) throw new ApiError(404, 'Email Not Found')
+    if (bcrypt.compareSync(password, user.password)) {
+        const token = jwt.sign({ email: user.email, password: user.password }, process.env.SECRET_KEY as string)
+        return { msg: 'Login successfully', token: token }
+    }
+    else throw new ApiError(403, 'Incorrect Authentication')
 }

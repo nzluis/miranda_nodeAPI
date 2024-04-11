570316d61ff67ad8370982ddea2150ceab43a325
luis <luisnavarroFS@gmail.com>
2024-04-11 13:12:03 +0200

    feat: new errorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 import { bookingsRouter } from './routes/booking'
 import { roomsRouter } from './routes/room'
 import { contactsRouter } from './routes/contact'
@@ -8,6 +8,7 @@ import dotenv from 'dotenv'
 import path from 'path'
 import { authMiddleware } from './middleware/auth'
 import mongoose from 'mongoose'
+import { errorHandler } from './utils/handleErrors'
 
 dotenv.config()
 
@@ -15,9 +16,10 @@ export const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL!).then(() => {
-    console.log('Connected to mongodb')
-})
+mongoose.connect(process.env.MONGODB_URL_TEST!)
+    .then(() => {
+        console.log('Connected to mongodb')
+    })
     .catch(() => {
         console.log('Error connecting mongodb')
     })
@@ -33,7 +35,4 @@ app.use('/rooms', authMiddleware, roomsRouter)
 app.use('/contacts', authMiddleware, contactsRouter)
 app.use('/users', authMiddleware, usersRouter)
 
-app.use((error: Error, _req: Request, res: Response, _next: NextFunction) => {
-    console.error(error)
-    return res.status(500).json({ message: 'Error from Server', error: error })
-})
+app.use(errorHandler)
--- src/utils/handleErrors.ts
+++ src/utils/handleErrors.ts
@@ -1,9 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+
 export class ApiError extends Error {
-    status: number
+    statusCode: number
+    status: string
     message: string
-    constructor(status: number = 500, message: string = 'Internal Server Error') {
+    constructor(statusCode: number = 500, message: string = 'Internal Server Error') {
         super(message)
-        this.status = status;
+        this.statusCode = statusCode;
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'
         this.message = message
+        Error.captureStackTrace(this, this.constructor)
     }
+}
+
+const castErrorHandler = (err: any) => {
+    const message = `Invalid ${err.path}: ${err.value}.`
+    return new ApiError(400, message)
+}
+
+const duplicateErrorHandler = (err: any) => {
+    const value = err.message.match(/(["'])(\\?.)*?\1/)[0]
+    const message = `field value:${value} already exist. please use another`
+    return new ApiError(400, message)
+}
+
+export const validationErrorHandler = (err: any) => {
+    const errors = Object.values(err.errors).map((el: any) => el.message);
+    const message = `Invalid input data. ${errors.join(". ")}`;
+    return new ApiError(400, message);
+}
+
+export const errorHandler = (error: any, _req: Request, res: Response, _next: NextFunction) => {
+    error.statusCode = error.statusCode || 500
+    error.status = error.status || 'error'
+    error.message = error.message || 'Internal Server Error'
+    const fullError = error
+    if (error.name === "CastError") error = castErrorHandler(error)
+    if (error.code === 11000) error = duplicateErrorHandler(error)
+    if (error.name === "ValidationError") error = validationErrorHandler(error)
+    res.status(error.statusCode).json({
+        status: error.status,
+        error: fullError,
+        message: error.message,
+        stack: error.stack,
+    });
 }

ae4b90955014ae10aab4a34d0190831eb3e45fba
luis <luisnavarroFS@gmail.com>
2024-04-12 12:10:02 +0200

    fix: no password in token

--- src/services/login.ts
+++ src/services/login.ts
@@ -7,7 +7,7 @@ export const getUser = async (email: string, password: string) => {
     const user = await User.findOne({ email })
     if (!user) throw new ApiError(404, 'Email Not Found')
     if (bcrypt.compareSync(password, user.password)) {
-        const token = jwt.sign({ email: user.email, password: user.password }, process.env.SECRET_KEY as string)
+        const token = jwt.sign({ email: user.email, id: user._id }, process.env.SECRET_KEY as string)
         return { msg: 'Login successfully', token: token }
     }
     else throw new ApiError(403, 'Incorrect Authentication')

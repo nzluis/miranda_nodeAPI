9248ee21300414011ca7474ac9d72982b6401e55
luis <luisnavarroFS@gmail.com>
2024-04-29 20:15:07 +0200

    feat: booking service

--- src/services/booking.ts
+++ src/services/booking.ts
@@ -1,31 +1,52 @@
 import { BookingData } from '../interfaces/Booking'
-import { Booking } from '../models/Booking'
 import { ApiError } from '../controllers/errorHandler'
+import { querySQL } from '../utils/sqlQuery'
 
 export const fetchAll = async () => {
-    const bookings = await Booking.find().populate('room')
-    if (!bookings) throw new ApiError(404, 'Data Not Found')
-    return bookings
+    return await querySQL(`SELECT * FROM booking`)
 }
 
 export const fetchOne = async (id: string) => {
-    const booking = await Booking.findById(id).populate('room')
-    if (!booking) throw new ApiError(404, 'Booking Id Not Found')
+    const booking = await querySQL(`SELECT * FROM booking WHERE _id = ?`, [id])
+    if (Object.keys(booking).length === 0) throw new ApiError(404, 'Booking Id Not Found')
     return booking
 }
 
 export const addNew = async (newAdded: BookingData) => {
-    return await new Booking(newAdded).save()
+    const newBooking = await querySQL(`INSERT INTO booking (
+        first_name, last_name, check_in, check_out, request, room, status) 
+        VALUES (?,?,?,?,?,?,?,?,?);`, [
+            newAdded.first_name,
+            newAdded.last_name,
+            newAdded.check_in,
+            newAdded.check_out,
+            newAdded.request,
+            newAdded.room,
+            newAdded.status,
+        ])
+    return newBooking
 }
 
 export const updateOne = async (id: string, updatedData: BookingData) => {
-    const editedBooking = await Booking.findByIdAndUpdate(id, updatedData, { new: true })
-    if (!editedBooking) throw new ApiError(404, 'Booking Id Not Found')
+    const foundBooking = await querySQL(`SELECT * FROM booking WHERE _id = ?;`, [id])
+    if (Object.keys(foundBooking).length === 0) throw new ApiError(404, 'Booking Id Not Found')
+    const editedBooking = await querySQL(`UPDATE booking
+        SET first_name = ?, last_name = ?, check_in = ?, check_out = ?, request = ?, room = ?, status = ? 
+        WHERE _id = ?;`, [
+            updatedData.first_name,
+            updatedData.last_name,
+            updatedData.check_in,
+            updatedData.check_out,
+            updatedData.request,
+            updatedData.room,
+            updatedData.status,
+            id
+        ])
     return editedBooking
 }
 
 export const deleteOne = async (id: string) => {
-    const deletedBooking = await Booking.findByIdAndDelete(id)
-    if (!deletedBooking) throw new ApiError(404, 'Booking Id Not Found')
+    const deletedBooking = await querySQL(`DELETE FROM booking WHERE _id = ?`, [id])
+    if (Object.keys(deletedBooking).length === 0) throw new ApiError(404, 'Booking Id Not Found')
     return deletedBooking
 }
